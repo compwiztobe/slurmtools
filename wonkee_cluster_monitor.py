8fa9d17b8a97a7a3d139eb340f61a5e377df6471
Aren Siekmeier <aren.siekmeier@gmail.com>
2021-08-10 17:48:13 +0900

    tweak cluster monitor script
    to show partition availability as well my own job counts, and pending job counts
    for all partition types and nodes

--- wonkee_cluster_monitor.py
+++ wonkee_cluster_monitor.py
@@ -1,42 +1,66 @@
-import sys
+#!/usr/bin/python
+
+import json
 import os
-from collections import defaultdict
+import sys
+from collections import Counter, OrderedDict
 
-#if len(sys.argv) != 2:
-#    print("Usage: python {} {}".format(os.path.basename(sys.argv[0]), 'FilePath'))
-#    exit
+with open(os.path.expanduser("~")+"/sinfo.json") as f:
+    sinfo = json.load(f, object_pairs_hook=OrderedDict)
 
-#f = open(sys.argv[1], 'r')
 lines = sys.stdin.readlines()
-#lines = f.readlines()
-pending=defaultdict(int)
-runGPU=defaultdict(int)
-runNode=defaultdict(int)
 
-for i, line in enumerate(lines):
-    if i == 0: continue
-    elif line.startswith('JOBID'): continue
+pending=Counter()
+running=Counter()
+nodes=Counter()
+
+me = os.getlogin()
+mypending=Counter()
+myrunning=Counter()
+mynodes=Counter()
+
+for line in lines:
+    if line.startswith('JOBID'):
+        continue
     line = line.strip().split()
+    user = line[3]
+    GPU = line[5]
+    numGPU = line[9].split(':')[-1]
+    try:
+        numGPU = int(numGPU)
+    except:
+        numGPU = 0
     if line[2] == 'PENDING':
-        GPU = line[5]
-        numGPU = int(line[9].split(':')[-1])
         pending[GPU] += numGPU
+        if user == me:
+            mypending[GPU] += numGPU
     else:
+        running[GPU] += numGPU
         nodeNum = line[7]
-        GPU = line[5]
-        numGPU = int(line[9].split(':')[-1])
-        runNode[nodeNum] += numGPU
-        runGPU[GPU] += numGPU
-#f.close()
+        nodes[nodeNum] += numGPU
+        if user == me:
+            myrunning[GPU] += numGPU
+            mynodes[nodeNum] += numGPU
 
-print("****  [PENDING STATUS]  **** ")
-for GPU, num in sorted(pending.items()):
-    print("- {:<8}: {}".format(GPU, num))
+import re
 
-print("\n****  [RUNNING STATUS]  **** ")
-print(">> running gpus:")
-for GPU, num in sorted(runGPU.items()):
-    print("- {:<8}: {}".format(GPU, num))
-print("\n>> running nodes:")
-for node, num in sorted(runNode.items()):
-    print("- {:<4}: {}".format(node, num))
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(item):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', item[0]) ]
+
+print("**** [GPUs reserved] ****")
+print("   me / all jobs / total  -> available ( / pending )")
+print("")
+for GPU, num in sinfo['GPUs'].items():
+    print("- {:<8}: {:>2} / {:>2} / {:>2}  -> {:>2} / {:>2}".format(GPU, myrunning[GPU], running[GPU], num, num - running[GPU], pending[GPU]))
+print("")
+print("per node:")
+for node, num in sorted(sinfo['nodes'].items(),key=natural_keys):
+    print("- {:<4}: {:>2} / {:>2} / {:>2}  -> {:>2}".format(node, mynodes[node], nodes[node], num, num - nodes[node]))
