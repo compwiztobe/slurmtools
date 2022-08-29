3b59d46e0b4d794dbe30f4eed8c378c4a3184572
Aren Siekmeier <aren@deltax.ai>
2022-08-29 18:42:28 +0900

    some updates for the linux native version of these scripts
    without slurm, launch with exec and track by PID
    also want to check status of still running jobs, by checking logfiles with active PIDs

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT="$1"
+SCRIPTDIR="./${SCRIPT%${SCRIPT##*/}}"
 
 # CUDA_VISIBLE_DEVICES
 export CUDA_VISIBLE_DEVICES=$2
@@ -11,7 +12,9 @@ ARGS=("${@:3}")
 ARGSTXT=${3:+$(printf -- "-%s" "${ARGS[@]//-}")}
 ARGS=${3:+$(printf -- "\"%s\" " "${ARGS[@]}")}
 
-JOBNAME="${SCRIPT%.sh}"-gpus${CUDA_VISIBLE_DEVICES//,}"$ARGSTXT"
+SCRIPTNAME="${SCRIPT##*/}"
+JOBNAME="$SCRIPTNAME"-gpus${CUDA_VISIBLE_DEVICES//,}"$ARGSTXT"
+#JOBNAME="${SCRIPTNAME%.sh}"-gpus${CUDA_VISIBLE_DEVICES//,}"$ARGSTXT"
 
 SHEBANG=$(head -n1 "$SCRIPT")
 SHEBANG=${SHEBANG#\#!}
@@ -22,12 +25,39 @@ COMMAND="$SHEBANG \"$SCRIPT\" $ARGS"
 # mv "$JOBNAME.out" "$OUTFILE.out"
 # echo $COMMAND "&> \"$OUTFILE.out\""
 
-eval $COMMAND &> "$HOME/runlogs/$JOBNAME.out.tmp" &
+OUTFILE="$HOME/runlogs/$JOBNAME.out.tmp"
+
+date --iso=s >> "$OUTFILE"
+
+(echo; echo "Current working directory:"; pwd; echo) >> "$OUTFILE"
+
+if GITHASH=$(git -C "$SCRIPTDIR" rev-parse --short HEAD 2> /dev/null); then
+  echo "$SCRIPTDIR currently at commit $GITHASH" >> "$OUTFILE"
+  if output=$(git -C "$SCRIPTDIR" status --porcelain) && [ -z "$output" ]; then
+    (echo "Working directory is clean (no unstaged changes or untracked files)") >> "$OUTFILE"
+  else
+    (echo; echo "WARNING: Working directory is not clean! There are uncommited changes.") >> "$OUTFILE"
+  fi
+else
+  echo "No git repository found." >> "$OUTFILE"
+fi
+echo >> "$OUTFILE"
+
+if [[ ! -z $VIRTUAL_ENV ]]; then
+  (echo "Current virtual environment:"; echo "$VIRTUAL_ENV"; echo "(another may be activated later by your script)"; echo) >> "$OUTFILE"
+else
+  (echo "No virtual environment activated (yet)"; echo) >> "$OUTFILE"
+fi
+
+echo "$COMMAND" >> "$OUTFILE"
+eval $COMMAND &>> "$OUTFILE" &
+PARENT_PID=$!
 # $! is only the PID of the eval, its direct child is the shebang + script
 # and we presume the script has a single direct child that we want to kill
 # so that's the PID we use in the output file
-PARENT_PID=$!
-sleep 0.5 # to make sure python starts
-PYTHON_PID=$(pgrep -P $(pgrep -P $PARENT_PID))
+#sleep 0.5 # to make sure python starts
+#PYTHON_PID=$(pgrep -P $(pgrep -P $PARENT_PID))
+# or only go a single level if spawning python directly from the eval (as SCRIPT)
+PYTHON_PID=$(pgrep -P $PARENT_PID)
 echo $COMMAND "&> \"$HOME/runlogs/$JOBNAME.$PYTHON_PID.out\""
-mv "$HOME/runlogs/$JOBNAME.out.tmp" "$HOME/runlogs/$JOBNAME.$PYTHON_PID.out"
+mv "$OUTFILE" "$HOME/runlogs/$JOBNAME.$PYTHON_PID.out"
